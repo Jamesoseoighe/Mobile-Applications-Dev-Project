1ed51032fa0815c8a9eeefa3725ea73737bb8306
jamesoseoighe <G00420774@atu.ie>
2024-04-25 19:34:15 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DoneDeal',
+  webDir: 'www'
+};
+
+export default config;
